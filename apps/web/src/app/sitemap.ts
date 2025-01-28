0b7b1d6957d0c7a9155aaef2c21cf80851901499
elitalpa <elitalpa@gmail.com>
2025-01-28 02:32:10 +0100

    feat(seo): adding docs pages in sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,7 +1,15 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
+import { allDocs } from "content-collections";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const docsPages: MetadataRoute.Sitemap = allDocs.map((doc) => ({
+    url: `${siteConfig.baseUrl}/docs/${doc._meta.path.replace("\\", "/")}`,
+    lastModified: new Date(),
+    changeFrequency: "weekly",
+    priority: 0.5,
+  }));
+
   return [
     {
       url: `${siteConfig.baseUrl}`,
@@ -17,5 +25,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.8,
       images: [`${siteConfig.baseUrl}/opengraph-image.png`],
     },
+    ...docsPages,
   ];
 }
